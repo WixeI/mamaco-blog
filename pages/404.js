35b57567dec6562539eb3f71ce1c9cd01f5e5107
Paulo <pauloportelamartins@gmail.com>
2022-03-13 17:00:13 -0300

    Added 404 page

---
+++ pages/404.js
@@ -0,0 +1,7 @@
+/**
+ * - This generates a 404 page automatically
+ * - If you don't have this file, Next will use it's default 404 page
+ */
+export default function Custom404() {
+    return <h2>404 - Page Not Foundee</h2>
+}
